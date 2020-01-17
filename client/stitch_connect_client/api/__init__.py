c8de245379f95d64f23f2a9f5ca04457c4877494
Jonathan Drake <jdrake@narrativescience.com>
2020-01-16 19:51:49 -0800

    refactor

--- client/stitch_connect_client/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from stitch_connect_client.api.destinations_api import DestinationsApi
-from stitch_connect_client.api.sources_api import SourcesApi
-from stitch_connect_client.api.streams_api import StreamsApi
